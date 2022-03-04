aa5bf531a49bb3db61c1802d2d5a6b406b983811
camnewnham <camnewnham@gmail.com>
2022-03-04 13:46:34 +1100

    add type casting to deserialization

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -6,9 +6,9 @@ declare type Class<T> = AbstractClass & {
     new (...args: any[]): T;
 };
 export declare function encode(obj: object, objectType?: AbstractClass): Uint8Array;
-export declare function decode<T>(data: ArrayLike<number> | BufferSource, objectType: Class<T> | AbstractClass): any;
+export declare function decode<T>(data: ArrayLike<number> | BufferSource, objectType: Class<T> | AbstractClass): T;
 export declare function serialize(obj: object, objectType?: AbstractClass): {};
-export declare function deserialize<T>(data: any[] | {}, objectType: Class<T> | AbstractClass): any;
+export declare function deserialize<T>(data: any[] | {}, objectType: Class<T> | AbstractClass): T;
 export declare function key(index: number | string, collectionType?: AbstractClass): (target: any, propertyName: string) => void;
 export declare function union(key: number | string, objectType: AbstractClass): (baseType: Class<any>) => void;
 export {};
